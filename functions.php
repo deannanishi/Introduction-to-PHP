2d7c38252c1ce52df4897b30dda68dbb7d55cd42
Deanna Nishi <50335808+deannanishi@users.noreply.github.com>
2019-07-03 00:41:28 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
-<? php
+<?php
 /*
 +---+
 | 1 |
@@ -6,12 +6,14 @@
 Declare the function named greeting. When this function is called,
 it prints: "<p>Welcome to the well structured code.</p>";
 */
-
+function greeting() { 
+    echo "<p>Welcome to the well structured code.</p>";
+}
 /*
 Call the function.
 */
 
-
+greeting();
 
 
 
@@ -27,10 +29,15 @@ This function has 3 parameters: a, b and c.
 calculateArithmeticMean prints the arithmetic mean of a, b and c.
 */
 
+function calculateArithmeticMean($a, $b, $c) {
+    echo ($a + $b + $c) / 3 ;
+}
+
 /*
 Call the function with the values for paremeters.
 */
 
+calculateArithmeticMean(3,3,6);
 
 
 
@@ -47,6 +54,11 @@ This function has 1 parameter: price.
 calculateTotalPrice returns the total price after the tax of 13%.
 */
 
+$price = 200;
+function calculateTotalPrice($price)  {
+    return $price + ($price * 0.13);
+}
+
 /*
 Declare the variable total
 and assign it with the value 
@@ -54,11 +66,13 @@ that calculateTotalPrice outputs
 when it is called (with the value for price parameter).
 */
 
+$total = calculateTotalPrice(100);
+
 /*
 Print total.
 */
 
-
+echo $total;
 
 
 
@@ -80,11 +94,20 @@ global variables length and width.
 Make these variables accessible inside the local scope, and do the calculation and printing
 */
 
+$length = 12;
+$width = 6;
+
+function calculateRectangleArea($length , $width) {
+    $loc = $length * $width;
+    $GLOBALS["loc"] = $loc;
+}
+
 /*
 Call the function.
 */
 
-
+calculateRectangleArea(100 , 5);
+echo $loc;
 
 
 
@@ -97,19 +120,24 @@ echo "<hr style=\"margin 1rem 0\">";
 +---+
 If the function for calculating the area of a triangle looks like this:
 */
-function calculateTriangleArea($base, $height) {
-    $area =  ($base * $height) / 2;
-}
+//function calculateTriangleArea($base, $height) {
+   // $area =  ($base * $height) / 2;}
 
 /*
 What can you do to make the $area accessible in global scope 
 (without using the return statement)?
 */
 
+function calculateTriangleArea($base , $height) {
+    $area =  ($base * $height) / 2;
+    $GLOBALS["area"] = $area;
+}
+
 /*
 Choose the values for parameters and call the function.
 */
 
-Print $area.
-    
+//Print $area;
+calculateTriangleArea(10, 15);
+echo $area;
 ?>
